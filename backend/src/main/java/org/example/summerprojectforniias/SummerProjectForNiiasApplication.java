67ac30d0719105c87315db939eb3cab6d2e39e22
danyaiv <danyaiv1337@gmail.com>
2026-07-08 16:18:56 +0300

    Initial commit: organized project structure with backend and docker-compose

---
+++ backend/src/main/java/org/example/summerprojectforniias/SummerProjectForNiiasApplication.java
@@ -0,0 +1,13 @@
+package org.example.summerprojectforniias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerProjectForNiiasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummerProjectForNiiasApplication.class, args);
+    }
+
+}
